251aff1e0ba8a6680abab4f2124eccd903dad340
KXIN1 <102916777+KXIN1@users.noreply.github.com>
2022-04-10 19:35:45 +0700

    เมนู

--- menu.php
+++ menu.php
@@ -14,7 +14,7 @@
     <title>index Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- icon ตะกร้า -->
-    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="CSS/menu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
   </head>
