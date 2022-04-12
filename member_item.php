a3d0477f37ca4345c39ba844d4f79ed90e8d1ac8
KXIN1 <102916777+KXIN1@users.noreply.github.com>
2022-04-13 00:40:39 +0700

    a

--- member_item.php
+++ member_item.php
@@ -13,7 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Item Page</title>
-    <link rel="stylesheet" href="CSS/item.css">
+    <link rel="stylesheet" href="CSS/item_t.css">
 </head>
 <body>
 
