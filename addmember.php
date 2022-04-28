2b49e10815398db26d887992aa6067df4c46f6a1
KXIN1 <102916777+KXIN1@users.noreply.github.com>
2022-04-28 17:11:31 +0700

    up file pic

--- addmember.php
+++ addmember.php
@@ -28,7 +28,7 @@
             $passwordenc = md5($password);
             
             $query = "INSERT INTO user (username, password, firstname, lastname, image, userlevel) 
-            VALUE ('$username', '$passwordenc', '$firstname', '$lastname', '0001.jpg', '$userlevel')";
+            VALUE ('$username', '$passwordenc', '$firstname', '$lastname', '001.jpg', '$userlevel')";
             $result = mysqli_query($conn, $query);
 
             if ($result) {
